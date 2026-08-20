c2f3b9bb872eba2cc4b36c9d32f44b17fe78fc63
Mark Korzin <mkorzin@Marks-Mac-mini.local>
2026-08-20 21:32:49 +0300

    bug fixes

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const calcGrid = document.querySelector('.calculator__grid');
 const calcDisplay = document.querySelector('.calculator__display');
 const clearBtn = document.querySelector('.btn--clear');
 let result = 0;
+let isFirstMinus = true;
+let isPressedEqualBtn = false;
 
 calcGrid.addEventListener('click', (e) => {
     const btn = e.target.closest('button');
@@ -12,7 +14,7 @@ calcGrid.addEventListener('click', (e) => {
     const key = btn.dataset.key;
 
     if (Number.isInteger(+key)) {
-        if (calcDisplay.textContent === 'AC') {
+        if (clearBtn.textContent === 'AC') {
             clearBtn.textContent = 'C';
         }
 
@@ -25,21 +27,94 @@ calcGrid.addEventListener('click', (e) => {
     } else if (key === 'clear') {
         clearBtn.textContent = 'AC';
         calcDisplay.textContent = '0';
+        result = 0;
+        console.clear();
 
     } else if (key === 'add') {
         if (!calcDisplay.textContent.endsWith('+')) {
-            const match = calcDisplay.textContent.match(/\d+$/);
-            const lastNumber = match ? match[0] : null;
+            const lastNumber = getLastNumber();
+            const lastSign = getLastSign();
 
-            calcDisplay.textContent += '+';
+            if (lastSign === '+') {
+                result += +lastNumber;
+            } else if (lastSign === '-') {
+                calcDisplay.textContent = calcDisplay.textContent.replace(/-$/, '+');
 
-            result += +lastNumber;
+                if (isFirstMinus) {
+                    result = +lastNumber;
+                } else {
+                    result -= +lastNumber;
+                }
+            } else {
+                result += +lastNumber;
+                calcDisplay.textContent += '+';
+            }
+
+            if (isPressedEqualBtn) {
+                result = 0;
+                isPressedEqualBtn = false;
+            }
 
             console.log(result);
         }
+
+    } else if (key === 'subtract') {
+        if (!calcDisplay.textContent.endsWith('-')) {
+            const lastNumber = getLastNumber();
+            const lastSign = getLastSign();
+
+            if (lastSign === '-') {
+                if (isFirstMinus) {
+                    result = +lastNumber;
+                } else {
+                    result -= +lastNumber;
+                }
+            } else if (lastSign === '+') {
+                result += +lastNumber;
+                calcDisplay.textContent = calcDisplay.textContent.replace(/\+$/, '-');
+            } else {
+                if (isFirstMinus) {
+                    result = +lastNumber;
+                } else {
+                    result -= +lastNumber;
+                }
+
+                calcDisplay.textContent += '-';
+            }
+
+            if (isFirstMinus) {
+                isFirstMinus = false;
+            }
+
+            console.log(result);
+        }
+
     } else if (key === 'equals') {
-        const lastSign = calcDisplay.textContent.match(/[-+*/]/g)?.at(-1) || null;
+        const lastSign = getLastSign();
+        const lastNumber = getLastNumber();
+
+        if (lastSign === '+') {
+            result += +lastNumber;
+        } else if (lastSign === '-') {
+            result -= +lastNumber;
+        }
 
         calcDisplay.textContent = result;
+
+        isPressedEqualBtn = true;
+        isFirstMinus = true;
+
+        console.log(result);
     }
 });
+
+function getLastNumber() {
+    const match = calcDisplay.textContent.match(/\d+$/);
+    const lastNumber = match ? match[0] : null;
+    return lastNumber;
+}
+
+function getLastSign() {
+    const lastSign = calcDisplay.textContent.match(/[-+*/]/g)?.at(-1) || null;
+    return lastSign;
+}
